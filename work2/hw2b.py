73e6177eb4b6eea2845f741a59962ea6e704ed20
Srikanth Narayanan <srikanth.n.narayanan@gmail.com>
2015-03-22 11:41:41 -0700

    Updated hw2b with cubic interpolation fucntion

--- work2/hw2b.py
+++ work2/hw2b.py
@@ -39,21 +39,6 @@ def quad_interp(xi,yi):
 
     return c
 
-
-def test_quad1():
-    """
-    Test code, no return value or exception if test runs properly.
-    """
-    xi = np.array([-1.,  0.,  2.])
-    yi = np.array([ 1., -1.,  7.])
-    c = quad_interp(xi,yi)
-    c_true = np.array([-1.,  0.,  2.])
-    print "c =      ", c
-    print "c_true = ", c_true
-    # test that all elements have small error:
-    assert np.allclose(c, c_true), \
-        "Incorrect result, c = %s, Expected: c = %s" % (c,c_true)
-
 def plot_quad(xi, yi):
     """
     A function to calculate and plot the quadratic interpolation of give array xi and yi.
@@ -83,7 +68,86 @@ def plot_quad(xi, yi):
     
     plt.title("Data points and interpolating polynomial")
     
-    plt.savefig('hw2b.png')   # save figure as .png file 
+    plt.savefig('hw2b.png')   # save figure as .png file
+
+def cubic_interp(xi,yi):
+    """
+    A method to perform cubic interpolation.
+    Returns c, an array containing the coefficients of
+    p(x) = c[0] + c[1]*x + c[2]*x**2 + c[3]*x**3
+    
+    :param xi: A numpy one dimensional array of length atleast 4
+    :param yi: A numpy one dimentional array of length atleast 4
+    """
+    # check inputs and print error message if not valid:
+
+    error_message = "xi and yi should have type numpy.ndarray"
+    assert (type(xi) is np.ndarray) and (type(yi) is np.ndarray), error_message
+
+    error_message = "xi and yi should have length 4"
+    assert len(xi)==4 and len(yi)==4, error_message
+    
+    # Set up linear system to interpolate through data points:
+    # co-efficient matrix for the system
+    A = np.vstack([np.ones((4)), xi, xi**2, xi**3])
+    b = yi
+    
+    ### compute c ###
+    c = solve(A,b)
+    
+    return c
+
+def plot_cubic(xi,yi):
+    """
+    This function accepts a set of xi and yi as input points to create a cubic
+    spline and plots the image of the spline.
+    
+    :param xi: A numpy one dimensional array of length atleast 4
+    :param yi: A numpy one dimentional array of length atleast 4
+    """
+    # check inputs and print error message if not valid:
+
+    error_message = "xi and yi should have type numpy.ndarray"
+    assert (type(xi) is np.ndarray) and (type(yi) is np.ndarray), error_message
+
+    error_message = "xi and yi should have length 4"
+    assert len(xi)==4 and len(yi)==4, error_message
+    
+    # calculate the co-efficients of the polynomial
+    c = cubic_interp(xi,yi)
+    print c
+    
+    # plot the resulting polynomial
+    x = np.linspace(xi.min() - 1, xi.max() + 1, 1000)
+    y = c[0] + c[1]*x + c[2]*x**2 + c[3]*x**3
+    
+    plt.figure(1)       # open plot figure window
+    plt.clf()           # clear figure
+    plt.plot(x,y,'b-')  # connect points with a blue line
+    
+    # Add data points  (polynomial should go through these points!)
+    plt.plot(xi,yi,'ro')   # plot as red circles
+    plt.ylim(yi.min() - 1,yi.max() + 1)         # set limits in y for plot
+    
+    plt.title("Data points and interpolating cubic polynomial")
+    
+    plt.savefig('cubic.png')   # save figure as .png file
+    
+
+def test_quad1():
+    """
+    Test code, no return value or exception if test runs properly.
+    """
+    xi = np.array([-1.,  0.,  2.])
+    yi = np.array([ 1., -1.,  7.])
+    c = quad_interp(xi,yi)
+    c_true = np.array([-1.,  0.,  2.])
+    print "c =      ", c
+    print "c_true = ", c_true
+    # test that all elements have small error:
+    assert np.allclose(c, c_true), \
+        "Incorrect result, c = %s, Expected: c = %s" % (c,c_true)
+    
         
 if __name__=="__main__":
     # "main program"
