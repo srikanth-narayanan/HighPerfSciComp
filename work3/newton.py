828789de971dfd6cca4745f434204017bfddf4bf
Srikanth Narayanan <srikanth.n.narayanan@gmail.com>
2015-03-26 20:04:00 -0700

    Created New python module called newton to solve square root of a number using newtons method

---
+++ work3/newton.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+"""
+This module contains class that can find the square root of a given number
+using newtons method.
+
+This module also contains some test function to test the convergence of the
+given number.
+
+:date: 26 Mar, 2015
+"""
+from __future__ import division
+
+__version__ = "0.1.0"
+__author__ = "Srikanth Narayanan"
+
+#import necessary libraries
+from numpy import sqrt
+
+
+
+class Newton(object):
+    """
+    This class creates an object that solves the square root of a given number
+    using newtons method
+    """
+    def __init__(self, val, initial_guess=1, itr=20, tol=1e-14, debug=False):
+        """
+        This is a constructor to load the object with a given number.
+
+        :param val: The val whose square to be calculated.
+        :param itr: number of iterations, Default value can be overidden.
+        :param tol: Tolerance value to be met by the solver
+        """
+        self.val = val
+        self.itr = itr
+        self.tol = tol
+        self.debug = debug
+        self.ini_gus = initial_guess
+    
+    def find_sqr_root(self):
+        """
+        This method uses new method to find roots
+        """
+        # set initial guess
+        x = self.ini_gus
+        
+        if self.debug:
+            print "Initial guess: x = %22.15E" % x
+        
+        # Newton iteration to find a zero of f(x) 
+        for ii in range(self.itr):
+            # evaluate func and derivative
+            fx = self.f_sqrt(x)
+            fxprime = self.fprime_sqrt(x)
+            if abs(fx) < self.tol:
+                self.sqrt = x
+                self.iter_consumed = ii + 1
+                return x, self.iter_consumed
+                break
+
+            # compute Newton increment x
+            deltax = fx/fxprime
+            # update x
+            x = x - deltax
+            
+            if self.debug:
+                print "After %d iterations, x = %22.15E" %(ii+1, x)
+        
+        # check for convergence
+        if ii == self.itr:
+            fx = self.f_sqrt(x)
+            if abs(fx) > self.tol:
+                print "*** Warning: has not yet converged"
+        
+        self.iter_consumed = ii + 1
+        return x, self.iter_consumed
+    
+    def f_sqrt(self, x):
+        """
+        This method defines the square root function
+        """
+        return x**2 - self.val
+    
+    def fprime_sqrt(self, x):
+        """
+        This method defines the derivative of the f_sqrt function
+        """
+        return 2*x
+
+    def test1(self):
+        """
+        Test Newton iteration for the square root with different initial
+        conditions.
+        """
+        self.val = 4
+        self.debug = True
+        for x0 in [1., 2., 100.]:
+            print " "  # blank line
+            self.ini_gus = x0
+            x,iters = self.find_sqr_root()
+            print "solve returns x = %22.15e after %i iterations " % (x,iters)
+            fx = self.f_sqrt(x)
+            fpx = self.fprime_sqrt(x)
+            print "the value of f(x) is %22.15e" % fx
+            assert abs(x-2.) < 1e-14, "*** Unexpected result: x = %22.15e"  % x
